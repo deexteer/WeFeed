41cf9bd096a660bb09a8b3936c82bf2e31830d05
ThomasOumar <oumar.thomas1998@gmail.com>
2018-03-13 09:52:47 +0100

    Add files via upload

--- WeFeed/public/app/lang/fr-lang.php
+++ WeFeed/public/app/lang/fr-lang.php
@@ -16,6 +16,23 @@
 
     //Fin publichome (français)
 
+	//Début connexion (français)
+
+    define('CONNEXION_TITLE', 'Connexion');
+    define('MAIL_INPUT', 'Votre Mail');
+    define('PASSWORD_INPUT', 'Votre mot de passe');
+    define('CONNECT_BUTTON', 'Se connecter');
+    define('INSCRIPTION_PAGE_REDIRECTION', 'Pas de compte ? Inscrivez-vous !');
+
+    //Fin connexion (français)
+
+	//Début inscription (français)
+
+    define('PUBLICHOME_SUBTITLE', 'Arrêtez de gâcher du papier');
+    define('PUBLICHOME_MOST_POPULAR', 'Votre mot de passe');
+
+    //Fin inscription (français)
+
 
 	//Début userhome (français)
 	define('POPUP_MESSAGE_DELCOL', 'Êtes-vous sûr de vouloir supprimer cette sources d\'informations ?');
