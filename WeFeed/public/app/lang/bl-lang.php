4579319d816dbf898b3da75a2d3f96148c3105cc
deexteer <bozhidar_ivanov@hotmail.com>
2018-03-13 10:56:37 +0200

    Update bl-lang.php

--- WeFeed/public/app/lang/bl-lang.php
+++ WeFeed/public/app/lang/bl-lang.php
@@ -2,20 +2,20 @@
 
     //Début header.php (anglais)
 
-    define('SEARCHBAR_TEXT', 'Search');
-    define('TXT_CONNECTED_AS', 'Connected as');
-    define('BTN_CONNECTION', 'Log in');
-    define('BTN_INSCRIPTION', 'Sign up');
+    define('SEARCHBAR_TEXT', 'Търсене');
+    define('TXT_CONNECTED_AS', 'Влязъл като');
+    define('BTN_CONNECTION', 'Влез');
+    define('BTN_INSCRIPTION', 'Регистрирай се');
 
     //Fin header.php (anglais)
 
 	//Début publichome (français)
 
-    define('PUBLICHOME_SUBTITLE', 'Stop wasting paper');
-    define('PUBLICHOME_MOST_POPULAR', 'MOST POPULAR');
+    define('PUBLICHOME_SUBTITLE', 'Щади хартията');
+    define('PUBLICHOME_MOST_POPULAR', 'НАЙ-ПОПУЛЯРНО');
 
     //Fin publichome (français)
 
 
 	//Début userhome (français)
-	define('POPUP_MESSAGE_DELCOL', 'Do you really want to delete this source ?');
+	define('POPUP_MESSAGE_DELCOL', 'Наистина ли искате да изтриете този източник ?');
