e69cfa78609740dc604afc885659e625594e5205
ThomasOumar <oumar.thomas1998@gmail.com>
2018-03-13 10:35:21 +0100

    Add files via upload

--- WeFeed/public/app/lang/en-lang.php
+++ WeFeed/public/app/lang/en-lang.php
@@ -9,13 +9,47 @@
 
     //Fin header.php (anglais)
 
-	//Début publichome (français)
+	//Début publichome (anglais)
 
     define('PUBLICHOME_SUBTITLE', 'Stop wasting paper');
     define('PUBLICHOME_MOST_POPULAR', 'MOST POPULAR');
 
-    //Fin publichome (français)
+    //Fin publichome (anglais)
 
+	//Début connexion (anglais)
+
+    define('CONNEXION_TITLE', 'Log in');
+    define('CON_MAIL_INPUT', 'Your mail address');
+    define('CON_PASSWORD_INPUT', 'Your password');
+    define('CONNECT_BUTTON', 'Sign in');
+    define('INSCRIPTION_PAGE_REDIRECTION', 'Don\'t have an account yet ?');
+
+    //Fin connexion (anglais)
+
+	//Début inscription (anglais)
+
+    define('INSCRIPTION_TITLE', 'Registration');
+    define('INSC_PSEUDO_INPUT', 'Pseudo');
+    define('INSC_MAIL_INPUT', 'Your mail address');
+    define('INSC_CONFIRM_MAIL_INPUT', 'Confirm your mail address');
+    define('INSC_PASSWORD_INPUT', 'Your password');
+    define('INSC_CONFIRM_PASSWORD_INPUT', 'Confirm your password');
+    define('INSCRIPTION_BUTTON', 'Sign up');
+    define('CONNEXION_PAGE_REDIRECTION', '
+Already registered ? Connect!');
+
+    //Fin inscription (anglais)
+
+	//Début settings (anglais)
+
+    define('SETTINGS_TITLE', 'Settings');
+    define('ADD_SOURCES_SELECT', 'Add a source of information');
+    define('CHANGE_PSEUDO', 'Change your pseudo');
+    define('CHANGE_MAIL', 'Change your mail address');
+    define('VALIDATE_BUTTON_SETTINGS', 'Validate');
+
+    //Fin settings (anglais)
 
 	//Début userhome (français)
+	
 	define('POPUP_MESSAGE_DELCOL', 'Do you really want to delete this source ?');
--- WeFeed/public/app/lang/fr-lang.php
+++ WeFeed/public/app/lang/fr-lang.php
@@ -26,13 +26,42 @@
 
     //Fin connexion (français)
 
-	//Début inscription (français)
+	//Début inscription (anglais)
 
-    define('PUBLICHOME_SUBTITLE', 'Arrêtez de gâcher du papier');
-    define('PUBLICHOME_MOST_POPULAR', 'Votre mot de passe');
+	define('INSCRIPTION_TITLE', 'S\'inscrire');
+	define('INSC_PSEUDO_INPUT', 'Pseudo');
+	define('INSC_MAIL_INPUT', 'Votre Mail');
+	define('INSC_CONFIRM_MAIL_INPUT', 'Confirmez votre mail');
+	define('INSC_PASSWORD_INPUT', 'Votre mot de passe');
+	define('INSC_CONFIRM_PASSWORD_INPUT', 'Confirmez votre mot de passe');
+	define('INSCRIPTION_BUTTON', 'S\'enregistrer');
+	define('CONNEXION_PAGE_REDIRECTION', 'Déjà inscrit ? Connectez-vous');
 
-    //Fin inscription (français)
+	//Fin inscription (anglais)
 
 
 	//Début userhome (français)
 	define('POPUP_MESSAGE_DELCOL', 'Êtes-vous sûr de vouloir supprimer cette sources d\'informations ?');
+
+	//Début connexion (anglais)
+
+    define('CONNEXION_TITLE', 'Connexion');
+    define('CON_MAIL_INPUT', 'Votre mail');
+    define('CON_PASSWORD_INPUT', 'Votre mot de passe');
+    define('CONNECT_BUTTON', 'Se connecter');
+    define('INSCRIPTION_PAGE_REDIRECTION', 'Vous n\'avez pas encore de compte ?');
+
+    //Fin connexion (anglais)
+
+	//Début settings (anglais)
+
+    define('SETTINGS_TITLE', 'Paramètres');
+    define('ADD_SOURCES_SELECT', 'Ajouter une nouvelle source d\'information');
+    define('CHANGE_PSEUDO', 'Changer de pseudo');
+    define('CHANGE_MAIL', 'Changer d\'addresse mail');
+    define('VALIDATE_BUTTON_SETTINGS', 'Valider');
+
+    //Fin settings (anglais)
+
+	//Début userhome (français)
+	define('POPUP_MESSAGE_DELCOL', 'Êtes vous sur de vouloir supprimer cette source d\'information ?');
