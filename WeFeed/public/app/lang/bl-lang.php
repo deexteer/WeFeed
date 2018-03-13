d3b2dc74cf1795044d3f9f89b3e4c8d6ba4f91da
ThomasOumar <oumar.thomas1998@gmail.com>
2018-03-13 09:52:38 +0100

    Add files via upload

---
+++ WeFeed/public/app/lang/bl-lang.php
@@ -0,0 +1,21 @@
+<?php
+
+    //Début header.php (anglais)
+
+    define('SEARCHBAR_TEXT', 'Search');
+    define('TXT_CONNECTED_AS', 'Connected as');
+    define('BTN_CONNECTION', 'Log in');
+    define('BTN_INSCRIPTION', 'Sign up');
+
+    //Fin header.php (anglais)
+
+	//Début publichome (français)
+
+    define('PUBLICHOME_SUBTITLE', 'Stop wasting paper');
+    define('PUBLICHOME_MOST_POPULAR', 'MOST POPULAR');
+
+    //Fin publichome (français)
+
+
+	//Début userhome (français)
+	define('POPUP_MESSAGE_DELCOL', 'Do you really want to delete this source ?');
