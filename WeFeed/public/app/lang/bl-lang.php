b83a0f00a524e4f8e79e54d00ba3686cbdfd788b
deexteer <bozhidar_ivanov@hotmail.com>
2018-03-15 08:28:57 +0200

    Update bl-lang.php

--- WeFeed/public/app/lang/bl-lang.php
+++ WeFeed/public/app/lang/bl-lang.php
@@ -1,46 +1,46 @@
 <?php
 
     //Début header.php (anglais)
-    define('SEARCHBAR_TEXT', 'Search');
-    define('TXT_CONNECTED_AS', 'Connected as ');
-    define('BTN_CONNECTION', 'Log in');
-    define('BTN_INSCRIPTION', 'Sign up');
+    define('SEARCHBAR_TEXT', 'Търсене');
+    define('TXT_CONNECTED_AS', 'Влязъл като ');
+    define('BTN_CONNECTION', 'Вход');
+    define('BTN_INSCRIPTION', 'Регистрация');
     //Fin header.php (anglais)
 
 	//Début publichome (anglais)
-    define('PUBLICHOME_SUBTITLE', 'Stop wasting paper');
-    define('PUBLICHOME_MOST_POPULAR', 'MOST POPULAR');
-    define('PUBLICHOME_MOST_LIKED', 'MOST LIKED');
+    define('PUBLICHOME_SUBTITLE', 'Щади хартията');
+    define('PUBLICHOME_MOST_POPULAR', 'НАЙ-ПОПУЛЯРНО');
+    define('PUBLICHOME_MOST_LIKED', 'НАЙ-ХАРЕСВАНО');
     //Fin publichome (anglais)
 
 	//Début connexion (anglais)
-    define('CONNEXION_TITLE', 'Log in');
-    define('CON_MAIL_INPUT', 'Your mail address');
-    define('CON_PASSWORD_INPUT', 'Your password');
-    define('CONNECT_BUTTON', 'Sign in');
-    define('INSCRIPTION_PAGE_REDIRECTION', 'Don\'t have an account yet ?');
+    define('CONNEXION_TITLE', 'Вход');
+    define('CON_MAIL_INPUT', 'Вашият email адрес');
+    define('CON_PASSWORD_INPUT', 'Вашата парола');
+    define('CONNECT_BUTTON', 'Вход');
+    define('INSCRIPTION_PAGE_REDIRECTION', 'Все още нямаш потребител ?');
     //Fin connexion (anglais)
 
 	//Début inscription (anglais)
-    define('INSCRIPTION_TITLE', 'Registration');
-    define('INSC_PSEUDO_INPUT', 'Pseudo');
-    define('INSC_MAIL_INPUT', 'Your mail address');
-    define('INSC_CONFIRM_MAIL_INPUT', 'Confirm your mail address');
-    define('INSC_PASSWORD_INPUT', 'Your password');
-    define('INSC_CONFIRM_PASSWORD_INPUT', 'Confirm your password');
-    define('INSCRIPTION_BUTTON', 'Sign up');
+    define('INSCRIPTION_TITLE', 'Регистрация');
+    define('INSC_PSEUDO_INPUT', 'Парола');
+    define('INSC_MAIL_INPUT', 'Вашият e-mail адрес');
+    define('INSC_CONFIRM_MAIL_INPUT', 'Потвърдете Вашият e-mail адрес');
+    define('INSC_PASSWORD_INPUT', 'Вашата парола');
+    define('INSC_CONFIRM_PASSWORD_INPUT', 'Потвърдете Вашата парола');
+    define('INSCRIPTION_BUTTON', 'Вход');
     define('CONNEXION_PAGE_REDIRECTION', '
-Already registered ? Connect!');
+Регистриран ? Влез!');
     //Fin inscription (anglais)
 
 	//Début userhome (français)
-	define('POPUP_MESSAGE_DELCOL', 'Do you really want to delete this source ?');
+	define('POPUP_MESSAGE_DELCOL', 'Наистина ли желаете да изтриете този източник ?');
 	//Fin userhome (français)
 
 	//Début settings (anglais)
-    define('SETTINGS_TITLE', 'Settings');
-    define('ADD_SOURCES_SELECT', 'Add a source of information');
-    define('CHANGE_PSEUDO', 'Change your pseudo');
-    define('CHANGE_MAIL', 'Change your mail address');
-    define('VALIDATE_BUTTON_SETTINGS', 'Validate');
+    define('SETTINGS_TITLE', 'Настройки');
+    define('ADD_SOURCES_SELECT', 'Добави източник на информация');
+    define('CHANGE_PSEUDO', 'Смени парола');
+    define('CHANGE_MAIL', 'Смени e-mail');
+    define('VALIDATE_BUTTON_SETTINGS', 'Валидирай');
     //Fin settings (anglais)
